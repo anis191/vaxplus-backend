247dac986de3218dc276a2c885112190da3ecf0b
anisul alam <anisulalam2002@gmail.com>
2025-09-14 14:57:06 +0600

    fix: change booking dates logics

--- booking/services.py
+++ booking/services.py
@@ -6,24 +6,39 @@ from rest_framework.exceptions import ValidationError
 
 class BookingServices:
 
+    # @staticmethod
+    # def get_available_dates(campaign):
+        # start = max(date.today(), campaign.start_date)
+        # end = campaign.end_date
+        # available_dates = []
+        # current = start
+        # if (end-start).days < 10:
+            # date_str = end.strftime('%Y-%m-%d')
+            # label = end.strftime('%B %d, %Y')
+            # available_dates.append((date_str, label))
+            # return available_dates
+        # else:
+            # while current <= end:
+                # date_str = current.strftime('%Y-%m-%d')
+                # label = current.strftime('%B %d, %Y')
+                # available_dates.append((date_str, label))
+                # current += timedelta(days=10)
+            # return available_dates
     @staticmethod
     def get_available_dates(campaign):
         start = max(date.today(), campaign.start_date)
         end = campaign.end_date
         available_dates = []
+
         current = start
-        if (end-start).days < 10:
-            date_str = end.strftime('%Y-%m-%d')
-            label = end.strftime('%B %d, %Y')
+        while current <= end:
+            date_str = current.strftime('%Y-%m-%d')
+            label = current.strftime('%B %d, %Y')
             available_dates.append((date_str, label))
-            return available_dates
-        else:
-            while current <= end:
-                date_str = current.strftime('%Y-%m-%d')
-                label = current.strftime('%B %d, %Y')
-                available_dates.append((date_str, label))
-                current += timedelta(days=10)
-            return available_dates
+            current += timedelta(days=1)
+
+        return available_dates
+
     
     @staticmethod
     def create_booking_dose(validated_data, campaign_id, user):
